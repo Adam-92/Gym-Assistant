41a218b79758cf2b2a47b86f470093df460ec2f8
Adam-92 <adam.kalcas2@gmail.com>
2022-02-10 09:40:13 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import '../style/index.css';
+import { useSelector, useDispatch } from "react-redux"
+import { bindActionCreators } from "redux"
+import { actionCreators }  from '../state/index'
+
+function App() {
+/*   const stateAccount = useSelector( (state) => state.account ) 
+     const dispatch = useDispatch()
+     const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch)
+*/
+  return (
+    <main className="main-container">
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+        
+      </section>
+    </main>
+  );
+}
+
+export default App;
