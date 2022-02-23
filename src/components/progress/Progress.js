0d44c2aed840b7486d7e38f1bf1317bf697d42d7
Adam-92 <adam.kalcas2@gmail.com>
2022-02-23 14:07:03 +0100

    Implemented mobile screen and grid in progress tab

---
+++ src/components/progress/Progress.js
@@ -0,0 +1,24 @@
+import './progress.css'
+
+const Progress = () => {
+    return(
+        <article className='charts-Progress'>
+            <section className="steps-Progress">
+
+            </section>
+            <section className="b-Progress">
+              
+            </section>
+            <section className="c-Progress">
+              asdad
+            </section>
+            <section className="d-Progress">
+              asd
+            </section>
+        </article>
+        
+
+    )
+}
+
+export default Progress
--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,21 +1,29 @@
 import './homePage.css'
 import SidebarNavigation from '../../components/sidebar-navigation/SidebarNavigation'
+import Progress from '../../components/progress/Progress'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
+const dateObject = new Date()
+const data = dateObject.toDateString()
+const time = `${dateObject.getHours()}:${dateObject.getMinutes()}`
+
 const HomePage = () => {
     return(
-        <main className="background-HomePage">
-            <article className="centerWindow-HomePage">
-                <div className="dashboard-HomePage">
-                  <div className="sidebar-HomePage">
-                    <SidebarNavigation/>
-                  </div>
-                  <div className="break-HomePage">
-
-                  </div>
-                  <div className="overwiew-HomePage">
-
-                  </div>
-                </div>
-            </article>
+        <main className="center-container-HomePage">
+            <div className="dashboard-HomePage">
+              <div className="sidebar-HomePage">
+                  <SidebarNavigation/>
+              </div>
+              <div className="overwiew-HomePage">
+{/*                   <header className='time-HomePage'>
+                      <FontAwesomeIcon icon={faClock} />
+                      <p>{data}</p>
+                      <p>{time}</p>
+                    </header> */}
+                    <Progress/>
+              </div>
+            </div>
         </main>
     )
 }
